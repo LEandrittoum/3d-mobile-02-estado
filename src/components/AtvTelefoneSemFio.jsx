83c51e06b9d2a57337d97483c1b0422f9ef59218
LEandrittoum <christian.moura@nave.org.br>
2026-09-01 09:42:05 -0300

    Update AtvTelefoneSemFio.jsx

--- src/components/AtvTelefoneSemFio.jsx
+++ src/components/AtvTelefoneSemFio.jsx
@@ -6,7 +6,7 @@ Este componente deve retornar uma <View> com dois <Pressable> lado a lado, e uma
 Cada <Pressable> ao ser clicado, deve inserir na <View> abaixo um <Text> e um <TextInput>, 
 e substituir o conteúdo anterior dessa <View> de baixo dos botões.
 O texto digitado no <TextInput> ativado por um <Pressable> deve ser exibido no <Text> do outro <Pressable>, e vice-versa.
-*/
+*/a
 
 import { View, Text, Pressable, StyleSheet, TextInput } from 'react-native'
 import { useState } from 'react'
