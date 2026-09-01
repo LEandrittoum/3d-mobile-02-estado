e07e014a202f50e1c2f78482922a507312d7865a
LEandrittoum <christian.moura@nave.org.br>
2026-09-01 09:40:24 -0300

    Update AtvDobra.jsx

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -4,7 +4,7 @@
 Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argumento "numero". 
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
-*/
+*/a
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import { useState } from 'react'
 
