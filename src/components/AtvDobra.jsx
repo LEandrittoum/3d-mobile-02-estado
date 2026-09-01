2938ddf58a474a97ebf25fae4895f6a3fffecacd
harieladorecife <mariaantonia.lima@nave.org.br>
2026-09-01 09:07:47 -0300

    Update AtvDobra.jsx

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,80 +5,66 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+// Elementos necessários para o componente
 
-import React, { useState } from "react";
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useState } from 'react'
 
-export default function AtvDobra({ numero }) {
-  const [valor, setValor] = useState(numero);
+// Estilos de "CSS"
+const estilo = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+})
 
-  return (
-    <View>
-      <Text>{valor}</Text>
+export default function AtvDobra() {
+  // Criação da variável "placar" e da função que altera seu valor
+  const [numero, setNumero] = useState(1)
 
-      <Pressable onPress={() => setValor(valor * 2)}>
-        <Text>Dobrar</Text>
-      </Pressable>
-    </View>
-  );
-}
+  // Funções que vão ser executadas pelos botões
+  function Dobrar() {
+    setNumero(numero * 2)
+  }
 
-
-import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable } from 'react-native';
-
-export default function App() {
-  const [nome, setNome] = useState('');
-
-  return (
-    <View style={{ padding: 20 }}>
-
-          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Pressable
-          onPress={() => setAtivo(1)}
-          style={{ borderWidth: "1px", padding: "15px" }}
-        >
-          <Text>{texto2 || 'Botão 1'}</Text>
-        </Pressable>
-
-        <Pressable
-          onPress={() => setAtivo(2)}
-          style={{ borderWidth: "1px", padding: "15px" }}
-        >
-          <Text>{texto1 || 'Botão 2'}</Text>
+  // Parte visual do componente
+  return(
+    <View style={estilo.view}>
+      <Text style={estilo.texto}>
+        {numero}
+      </Text>
+      <View style={estilo.viewBotoes}>
+        <Pressable style={estilo.botao} onPress={() => Dobrar()}>
+          <Text style={estilo.textoBotao}>dobrar</Text>
         </Pressable>
       </View>
-      <View style={{ marginTop: "30px" }}>
-        {ativo === 1 && (
-          <>
-            <Text>Fale algo para o botão 2:</Text>
-            <TextInput
-              value={texto1}
-              onChangeText={setTexto1}
-              style={{
-                borderWidth: "1px",
-                padding: "10px",
-                marginTop: "10px",
-              }}
-            />
-          </>
-        )}
-
-        {ativo === 2 && (
-          <>
-            <Text>Fale algo para o botão 1:</Text>
-            <TextInput
-              value={texto2}
-              onChangeText={setTexto2}
-              style={{
-                borderWidth: "1px",
-                padding: "10px",
-                marginTop: "10px",
-              }}
-            />
-          </>
-        )}
-      </View>
     </View>
-  );
+  )
 }
