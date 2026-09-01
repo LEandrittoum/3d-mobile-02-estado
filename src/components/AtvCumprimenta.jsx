c46b0d61f64a3213d3faa2c5ba397ff311290e83
SenaPaiva <mariaeduarda.sena@nave.org.br>
2026-08-31 22:35:52 -0300

    UpdatE

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,85 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+
+import { View, Text, Pressable, StyleSheet, TextInput } from 'react-native'
+import { useState } from 'react'
+
+
+export default function AtvCumprimenta(){
+ const [textoInserido, setTextoInserido] = useState('')
+ const [texto, setTexto] = useState('')
+
+
+  return(
+    <View style={estilo.view}>
+      <Text style={estilo.texto}>"Olá, qual seu nome?"</Text>
+      <TextInput
+        style={estilo.input}
+        value={textoInserido}
+        onChangeText={setTextoInserido}
+        placeholder="Digite aqui o seu nome"/>
+
+
+    <Pressable style={estilo.botao} onPress={() => setTexto(textoInserido)}>
+        <Text style={estilo.textoBotao}>Salvar nome</Text>
+      </Pressable>
+
+
+
+
+      {texto !== "" && (
+      <Text style={estilo.resposta}>É um prazer lhe conhecer, {texto}</Text>
+    )}
+    </View>
+  );
+}
+
+
+    const estilo = StyleSheet.create({
+
+
+  view: {
+    padding: 20,
+    width: '100%',
+  },
+  titulo: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  linhaInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#e7488a',
+    padding: 10,
+    marginRight: 10,
+    borderRadius: 4,
+  },
+  botao: {
+    backgroundColor: '#d65989',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textoBotao: {
+    color: '#691a38',
+  },
+  resposta: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+});
+
+
+
+
+
+
+
